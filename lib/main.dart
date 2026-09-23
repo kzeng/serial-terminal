9b47f8571e799bbbdb0e35f193705c0f80e971b2
Kai Zeng <Kai.Zeng@lumentum.com>
2026-09-23 10:51:57 +0800

    chore: bump version to 0.0.2

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_libserialport/flutter_libserialport.dart';
 
-const appVersion = '0.0.1';
+const appVersion = '0.0.2';
 
 void main() => runApp(const SerialTerminalApp());
 
